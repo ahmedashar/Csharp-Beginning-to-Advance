612a157e48cf2651266489e4e750d2a9eacf90b1
ahmedashar <ahmedashar12345@gmail.com>
2023-02-28 15:10:34 +0500

    add-abstraction

---
+++ ObjectOriented/Abstraction/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Abstraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+//Abstraction is an important part of object oriented programming.
+//*It means that only the required information is visible to the user and the rest of the information is hidden.
+//*Abstraction can be implemented using abstract classes in C#.
+//*Abstract classes are base classes with partial implementation.
+//*In C#, we cannot create objects of an abstract class
